5f6bd82962010dd772ee9ec516e798f69f21f5e7
IgorFZ <igorf.zamp@gmail.com>
2023-06-12 11:31:06 -0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+var form = document.getElementById('form');
+var text = document.getElementById('text');
+
+var response = document.getElementById('response');
+
+form.addEventListener('submit', function(e) {
+    response.innerHTML = `<p>${text.value}</p>`;
+    e.preventDefault();
+});
